0a71918287e8d7cde78c7f9d68ef69b19d88d799
Jao025 <joaosimoes0225@gmail.com>
2025-02-23 18:41:08 -0300

    trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -15,6 +15,6 @@ sudo mv -f beginner-html-site-styled/* /var/www/html/ && echo "Arquivos movidos 
 sudo systemctl enable httpd && echo "Apache ativado para iniciar com o sistema" >> $LOG_FILE
 sudo systemctl start httpd && echo "Apache iniciado" >> $LOG_FILE
 
-curl -X POST -d "nome=João Victor de Almeida" https://difusaotech.com.br/lab/aws/index.php && echo "Nome no site" >> $LOG_FILE
+curl -X POST -d "nome=João Victor de Almeida" https://difusaotech.com.br/lab/aws/index.php
 
 echo "Instalação concluída!" >> $LOG_FILE
